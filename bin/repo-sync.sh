fd34c59a5dd5b8cf4853fd09c54753fb9741cac3
Dan Schultz <slifty@gmail.com>
2023-01-09 15:31:53 -0500

    Add stela to auto-sync
    The `stela` repository is a new addition to our development environment
    and it seems important to keep it up to date as well.
    
    This equally shortens the repo-sync script.

--- bin/repo-sync.sh
+++ bin/repo-sync.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
+function checkout_and_update_main {
+    git checkout main
+    git pull origin main
+}
 
-git checkout main
-git pull origin main
+checkout_and_update_main
 cd ../infrastructure/
-git checkout main
-git pull origin main
+checkout_and_update_main
 cd ../upload-service
-git checkout main
-git pull origin main
+checkout_and_update_main
 cd ../web-app
-git checkout main
-git pull origin main
+checkout_and_update_main
 cd ../back-end
-git checkout main
-git pull origin main 
-
+checkout_and_update_main
+cd ../stela
+checkout_and_update_main
